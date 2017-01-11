42eeed528cc9cd1a3f89efa39375f7ee197edd52
promise <promiseid@126.com>
2017-01-12 01:01:56 +0800

    php zero

--- 01-01/index.php
+++ 01-01/index.php
@@ -1,10 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+    $title_name='Promise';
+    ?>
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title><?php echo $title_name ?></title>
 </head>
 <body>
+    <?php
+        //helloo
+        $name = "promise";
+        echo "PHP ".$name;
+        $name = ' TongTong';
+        echo $name;
+        /*
+            ddd
+        */
+    ?>
 
+    <?php
+        $name = 'Promise';
+        echo "I'm $name";
+        echo "\n Hello World!"
+            //单引号内容都解析成字符串
+            //双引号内容隐式转换成相应类型
+    ?>
 </body>
 </html>
+
+<script>
+
+</script>
